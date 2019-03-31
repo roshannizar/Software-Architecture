868261530208b17b075810165d84853a8195c892
HK-Rathnayaka <43449972+HK-Rathnayaka@users.noreply.github.com>
2019-03-31 13:20:54 +0530

    Code Update 9

--- Producer/src/com/producer/ProducerActivator.java
+++ Producer/src/com/producer/ProducerActivator.java
@@ -13,31 +13,42 @@ public class ProducerActivator implements BundleActivator {
 	ServiceRegistration publishServiceRegistration;
 
 	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Producer Start");
-		sc= new Scanner(System.in);
+		System.out.println("Producer Start\n\n");
+		sc = new Scanner(System.in);
 		Flight publisherService;
-	
-		System.out.println("Flight Booking\n");
+
+		System.out.println(" Flight Booking System\n");
 		System.out.println("----------------------");
+
 		System.out.println("1. Emirates\n2. Sri Lankan Air Lines\n3. King Fisher\n4. Air Asia\n");
-		System.out.println("Select your prefered Flight Id: \n");
-		
+		System.out.print("Select your prefered Flight Id: ");
 		flightType = sc.nextInt();
 		
-		switch(flightType) {
-		
-			case 1: publisherService = new Emirates();
-					break;
-			case 2: publisherService = new Airlines();
-					break;
-			case 3: publisherService = new KingFisher();
-					break;
-			case 4: publisherService = new AirAsia();
-					break;
-			
-			default: publisherService = new Emirates();
+		while (flightType < 1 || flightType > 4) {
+			System.out.print("\nPlease select valid Flight Id: \n");
+			System.out.println("1. Emirates\n2. Sri Lankan Air Lines\n3. King Fisher\n4. Air Asia\n");
+			flightType = sc.nextInt();
 		}
-		
+
+		switch (flightType) {
+
+		case 1:
+			publisherService = new Emirates();
+			break;
+		case 2:
+			publisherService = new Airlines();
+			break;
+		case 3:
+			publisherService = new KingFisher();
+			break;
+		case 4:
+			publisherService = new AirAsia();
+			break;
+
+		default:
+			publisherService = new Emirates();
+		}
+
 		publishServiceRegistration = bundleContext.registerService(Flight.class.getName(), publisherService, null);
 	}
 
